714bcee9c3147637286f42892c335c027f981b0d
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-08-29 21:45:27 +0700

    Add GET /users/me and authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,26 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch(e) {
+    // return new Promise((resolve,reject) => {
+    //   reject();
+    // });
+    return Promise.reject(); // same with above
+  }
+  
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
+
 var User = mongoose.model('Users', UserSchema);
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -87,6 +88,12 @@ app.patch('/todos/:id', (req,res) => {
 
     res.send(todo);
   }, (err) => res.status(400).send());
+});
+
+
+
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user);
 });
 
 // POST /users
