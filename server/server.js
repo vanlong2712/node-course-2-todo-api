9c6ca23e6b5cb1fc2b13cbb2031b2268405cf1dd
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-08-29 16:38:22 +0700

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongo');
 var {Todo} = require('./models/todo');
@@ -61,6 +61,30 @@ app.delete('/todos/:id', (req, res) => {
     res.send(doc);
   }).catch((e) => res.status(400).send());
 
+});
+
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send(todo);
+  }, (err) => res.status(400).send());
 });
 
 app.listen(port, () => console.log(`Starting on port ${port}`));
