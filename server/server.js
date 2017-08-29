aec3253c804b41d9d68dff7dd0d8d3f94b07976a
DESKTOP-RM6ETRP\vanlong2712 <d.long271293@gmail.com>
2017-08-29 15:16:44 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongo');
 var {Todo} = require('./models/todo');
 var {Users} = require('./models/user');
@@ -25,6 +27,31 @@ app.get('/todos', (req,res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req,res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => res.send());
+  //valid id using isValid
+  //404 - send back empty send
+
+  // findById
+    // success case
+      // if todo - send it back
+      // if no todo - send back 404 with empty body
+    //error case
+      //400 - and send empty body back
 });
 
 app.listen(3000, () => console.log('Starting on port 3000'));
